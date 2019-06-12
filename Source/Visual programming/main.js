6b061660c937ee3b9f0ad4b80bf53ad8c019550c
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2019-06-12 23:57:19 +0200

    Visual: Blocks to text API

--- Source/Visual programming/main.js
+++ Source/Visual programming/main.js
@@ -74,7 +74,19 @@ document.getElementById("save-button").addEventListener("click", function()
     var xml = Blockly.Xml.workspaceToDom(workspace);
     var xml_text = Blockly.Xml.domToText(xml);
 
-    var blob = new Blob([xml_text],{type: "text/xml"});
+    download(xml_text, "blocks", "text/xml");
+});
+
+document.getElementById("download-button").addEventListener("click", function()
+{ 
+    textPanel.value;
+
+    download(text, "text.m", "text/text");
+});
+
+function download(text, name, type)
+{
+    var blob = new Blob([text],{type: type});
     var url = URL.createObjectURL(blob);
     var div = document.createElement("div")
     var anch = document.createElement("a");
@@ -84,14 +96,103 @@ document.getElementById("save-button").addEventListener("click", function()
 
     anch.innerHTML = "&nbsp;";
     anch.href = url;
-    anch.download = "blocks";
+    anch.download = name;
         
     var ev = new MouseEvent("click",{});
     anch.dispatchEvent(ev);
     document.body.removeChild(div);
-});
+}
 
 function blocksToText(event) 
 {
+    var result = "";
+    var blocks = workspace.getBlocksByType("entity", true);
+
+    for (var i = 0; i < blocks.length; i++)
+    {
+        var block = blocks[i];
+
+        if (block.getParent() == null)
+        {
+            result += EntityToCode(block, false) + "\n\n";
+        }
+
+    }
+
+    textPanel.value = result;
+}
+
+function EntityToCode(block, isSubEntity)
+{
+    var result = "";
+    var name = block.getFieldValue('NAME');
+    if (isSubEntity)
+    {
+        result += " " + name
+    }
+    else
+    {
+        result += "entity " + name
+    }
+    var base = block.getInputTargetBlock('base');
+    if (base != null)
+    {
+        var baseName = base.getFieldValue('NAME');
+        result += " based on " + baseName;
+
+        var removedComponent = base.getInputTargetBlock('removed');
+        if (removedComponent != null)
+        {
+            result += " without";
+        }
+        while (removedComponent != null)
+        {
+            var name = removedComponent.getFieldValue('NAME');
+            result += " " + name;
+            removedComponent = removedComponent.getNextBlock();
+        }
+    }
+
+    var component = block.getInputTargetBlock('components');
     
+    if (component != null)
+    {
+        result += " has";
+    }
+    while (component != null)
+    {
+        var componentName = component.getFieldValue('NAME');
+
+        if (component.type == "tag")
+        {
+            result += " " + componentName;
+        }
+        else if (component.type == "real1")
+        {
+            var x = component.getFieldValue('X');
+            result += " " + componentName + " " + x;
+        }
+        else if (component.type == "real2")
+        {
+            var x = component.getFieldValue('X');
+            var y = component.getFieldValue('Y');
+            result += " " + componentName + " " + x + " " + y;
+        }
+        component = component.getNextBlock();
+    }
+
+    var subEntity = block.getInputTargetBlock('subEntities');
+    
+    if (subEntity != null)
+    {
+        result += " contains";
+    }
+    while (subEntity != null)
+    {
+        result += EntityToCode(subEntity, true);
+        subEntity = subEntity.getNextBlock();
+    }
+    result += '.';
+
+    return result;
 }
