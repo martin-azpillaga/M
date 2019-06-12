3707a3744ff44404409b39016025c02547fffdec
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2019-06-12 21:25:35 +0200

    fixes #46 #43 #36

--- Source/Visual programming/main.js
+++ Source/Visual programming/main.js
@@ -1,55 +1,97 @@
-function onFileLoad(elementId, event) 
-{
-    var xml = Blockly.Xml.textToDom(event.target.result);
-    Blockly.Xml.domToWorkspace(xml, workspace);
-}
+var blocksPanel = document.getElementById("blocklyDiv");
+var textPanel = document.getElementById("textPanel");
+var workspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox'), zoom: {controls: true, wheel: true}});
+workspace.addChangeListener(blocksToText);
 
-function onChooseFile(event, onLoadFileHandler) {
-    if (typeof window.FileReader !== 'function')
-        throw ("The file API isn't supported on this browser.");
-    let input = event.target;
-    if (!input)
-        throw ("The browser does not properly implement the event object");
-    if (!input.files)
-        throw ("This browser does not support the `files` property of the file input.");
-    if (!input.files[0])
-        return undefined;
-    let file = input.files[0];
-    let fr = new FileReader();
-    fr.onload = onLoadFileHandler;
+document.getElementById("blocks-button").addEventListener("click", function()
+{
+    if (blocksPanel.style.display === "inline-block" || blocksPanel.style.display === "")
+    {
+        blocksPanel.style.display = "none"
+        if (textPanel.style.display === "inline-block" || textPanel.style.display==="")
+        {
+            textPanel.style.width = "100%";
+        }
+    }
+    else if (textPanel.style.display === "none")
+    {
+        blocksPanel.style.display = "inline-block"
+        blocksPanel.style.width = "100%";
+        Blockly.svgResize(workspace);
+    }
+    else
+    {
+        blocksPanel.style.display = "inline-block"
+        blocksPanel.style.width = "50%";
+        textPanel.style.width = "49%";
+        Blockly.svgResize(workspace);
+    }
+});
+document.getElementById("text-button").addEventListener("click", function()
+{
+    if (textPanel.style.display === "inline-block" || textPanel.style.display === "")
+    {
+        textPanel.style.display = "none"
+        if (blocksPanel.style.display === "inline-block" || blocksPanel.style.display==="")
+        {
+            blocksPanel.style.width = "100%";
+            Blockly.svgResize(workspace);
+        }
+    }
+    else if (blocksPanel.style.display === "none")
+    {
+        textPanel.style.display = "inline-block"
+        textPanel.style.width = "100%";
+    }
+    else
+    {
+        textPanel.style.display = "inline-block"
+        textPanel.style.width = "49%";
+        blocksPanel.style.width = "50%";
+        Blockly.svgResize(workspace);
+    }
+});
+
+document.getElementById("load-button").addEventListener("click", function()
+{
+    document.getElementById("uploaded-input").click();
+});
+
+document.getElementById("uploaded-input").addEventListener("change", function()
+{
+    var file = document.getElementById("uploaded-input").files[0];
+    var fr = new FileReader();
+    fr.onload = function(e)
+    {
+        var xml = Blockly.Xml.textToDom(e.target.result);
+        Blockly.Xml.domToWorkspace(xml, workspace);
+    };
     fr.readAsText(file);
-}
+});
 
-var workspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox'), scrollbars: false, zoom: {controls: true, wheel: true}});
-function myUpdateFunction(event) 
-{
-//alert("I am an alert box!");
-}
-function load()
-{
-alert("Alerta")
-}
-function save()
-{
-var xml = Blockly.Xml.workspaceToDom(workspace);
-var xml_text = Blockly.Xml.domToText(xml);
-var blob = new Blob([xml_text],{type: "text/xml"});
-var url = URL.createObjectURL(blob),
-            div = document.createElement("div"),
-            anch = document.createElement("a");
+document.getElementById("save-button").addEventListener("click", function()
+{ 
+    var xml = Blockly.Xml.workspaceToDom(workspace);
+    var xml_text = Blockly.Xml.domToText(xml);
 
-        document.body.appendChild(div);
-        div.appendChild(anch);
+    var blob = new Blob([xml_text],{type: "text/xml"});
+    var url = URL.createObjectURL(blob);
+    var div = document.createElement("div")
+    var anch = document.createElement("a");
 
-        anch.innerHTML = "&nbsp;";
-        div.style.width = "0";
-        div.style.height = "0";
-        anch.href = url;
-        anch.download = "workspace";
+    document.body.appendChild(div);
+    div.appendChild(anch);
+
+    anch.innerHTML = "&nbsp;";
+    anch.href = url;
+    anch.download = "blocks";
         
-        var ev = new MouseEvent("click",{});
-        anch.dispatchEvent(ev);
-        document.body.removeChild(div);
+    var ev = new MouseEvent("click",{});
+    anch.dispatchEvent(ev);
+    document.body.removeChild(div);
+});
 
+function blocksToText(event) 
+{
+    
 }
-workspace.addChangeListener(myUpdateFunction);
