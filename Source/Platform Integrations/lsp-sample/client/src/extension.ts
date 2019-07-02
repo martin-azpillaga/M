568553c551dd39dac877c21f32b8056a2485e982
Martin Azpillaga <martin.azpillaga@gmail.com>
2019-07-03 00:55:03 +0200

    VSCode integration working

---
+++ Source/Platform Integrations/lsp-sample/client/src/extension.ts
@@ -0,0 +1,52 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+
+	let serverLauncher = context.asAbsolutePath(path.join('xtext-server', 'bin', 'xtext-server'));
+	let serverOptions: ServerOptions = {
+		run : { command: serverLauncher }, debug: { command: serverLauncher }
+	};
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'm' },{ scheme: 'file', language: 'blocks'}],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'languageServerExample',
+		'Language Server Example',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
