f4d2fdb0611ea85912b39d2d473b2ac220efd1c7
Martin Azpillaga <martin.azpillaga@gmail.com>
2019-11-22 03:04:20 +0100

    Quality assurance script in python

---
+++ Quality/Quality.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import glob
+from subprocess import *
+
+xtendFiles = 100
+xtendLines = 10000
+xtendBlanks = 1000
+xtendComments = 0
+
+def main():
+    result = run(['scc', '..'], stdout=PIPE, stderr=PIPE)
+    stdout = result.stdout.decode('utf-8')
+    stderr = result.stderr.decode('utf-8')
+    
+    for line in stdout.split('\n'):
+        if (line.startswith('Xtend')):
+            entries = []
+            for entry in line.split(' '):
+                if (entry != ''):
+                    entries.append(entry)
+            
+            assert int(entries[1]) <= xtendFiles, "too many files"
+            assert int(entries[2]) <= xtendLines, "too many lines"
+            assert int(entries[3]) <= xtendBlanks, "too many blanks"
+            assert int(entries[4]) <= xtendComments, "too many comments"
+
+if __name__ == "__main__":
+    main()
