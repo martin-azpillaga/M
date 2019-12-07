4e06303e3576d3da9b9ce8a43e9ecc211c132a37
martin-azpillaga <martin.azpillaga@gmail.com>
2019-12-07 21:13:03 +0100

    Language server script downloads sources to lib/source and compiles java to bin/

--- Language server/script.sh
+++ Language server/script.sh
@@ -1,5 +1,6 @@
-[ ! -f lib/xtext.ide.jar ] &&
-curl https://search.maven.org/remotecontent?filepath=org/eclipse/xtext/org.eclipse.xtext.ide/2.20.0/org.eclipse.xtext.ide-2.20.0.jar -o lib/xtext.ide.jar
+[ ! -f lib/classes/xtext.ide.jar ] && curl https://search.maven.org/remotecontent?filepath=org/eclipse/xtext/org.eclipse.xtext.ide/2.20.0/org.eclipse.xtext.ide-2.20.0.jar -o lib/classes/xtext.ide.jar
 
-javac -cp "src/:lib/*" src/m/*/*.java
-java -cp "src/:lib/*" m.main.Main
+[ ! -f lib/source/xtext.ide.jar ] && curl https://search.maven.org/remotecontent?filepath=org/eclipse/xtext/org.eclipse.xtext.ide/2.20.0/org.eclipse.xtext.ide-2.20.0-sources.jar -o lib/source/xtext.ide.jar
+
+javac -d "bin" -cp "src/:lib/classes/*" src/m/*/*.java
+java -cp "bin/:lib/classes/*" m.main.Main
