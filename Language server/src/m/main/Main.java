cc96b251d17ede8801ee6ce6946e0857177680ec
martin-azpillaga <martin.azpillaga@gmail.com>
2019-12-07 20:54:48 +0100

    Language server scripts download dependency jars, compiles java and executes main with classpath

---
+++ Language server/src/m/main/Main.java
@@ -0,0 +1,12 @@
+package m.main;
+
+import org.eclipse.xtext.ide.server.LaunchArgs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var it = new LaunchArgs();
+
+        System.out.println(it);
+    }
+}
