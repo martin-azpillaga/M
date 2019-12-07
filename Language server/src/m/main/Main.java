8b6158d7de0e10f1ccce1f48f0319b763632b605
martin-azpillaga <martin.azpillaga@gmail.com>
2019-12-07 22:15:29 +0100

    google guice dependency

--- Language server/src/m/main/Main.java
+++ Language server/src/m/main/Main.java
@@ -1,12 +1,20 @@
 package m.main;
 
+import com.google.inject.Guice;
+
 import org.eclipse.xtext.ide.server.LaunchArgs;
+import org.eclipse.xtext.ide.server.ServerLauncher;
+import org.eclipse.xtext.ide.server.ServerModule;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         var it = new LaunchArgs();
-
-        System.out.println(it);
+        it.setIn(System.in);
+        it.setOut(System.out);
+        final ServerLauncher launcher = Guice.createInjector(new ServerModule()).
+        <ServerLauncher>getInstance(ServerLauncher.class);
+        launcher.start(it);
     }
 }
