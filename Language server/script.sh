774594eac6e9e7b7d791fcbd70eace3ae2e299af
martin-azpillaga <martin.azpillaga@gmail.com>
2019-12-08 00:20:00 +0100

    First attempt at checkstyle

--- Language server/script.sh
+++ Language server/script.sh
@@ -1,3 +1,9 @@
+[ ! -f script.sh ] && echo "Run from Language server folder" && exit 1
+
+[ ! -d lib ] && mkdir lib
+[ ! -d lib/classes ] && mkdir lib/classes
+[ ! -d lib/source ] && mkdir lib/source
+
 [ ! -f lib/classes/xtext.ide.jar ] && curl https://search.maven.org/remotecontent?filepath=org/eclipse/xtext/org.eclipse.xtext.ide/2.20.0/org.eclipse.xtext.ide-2.20.0.jar -o lib/classes/xtext.ide.jar
 
 [ ! -f lib/source/xtext.ide.jar ] && curl https://search.maven.org/remotecontent?filepath=org/eclipse/xtext/org.eclipse.xtext.ide/2.20.0/org.eclipse.xtext.ide-2.20.0-sources.jar -o lib/source/xtext.ide.jar
