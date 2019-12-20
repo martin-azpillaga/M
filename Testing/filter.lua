7a44711dc516f2f038ae13b14653a7102d252eb2
martin-azpillaga <martin.azpillaga@gmail.com>
2019-12-20 01:39:23 +0100

    Lua filter formatted

--- Testing/filter.lua
+++ Testing/filter.lua
@@ -1,18 +1,19 @@
 function CodeBlock(block)
+
     if block.classes[3] == nil then return end
 
     local action = block.classes[1]
     local workspace = block.classes[2]
     local file = block.classes[3]
     local path = "Tests/" .. workspace .. "/" .. file
-    
+
     if not Directory("Tests") then
         os.execute("mkdir Tests")
     end
-    if not Directory("Tests/" .. workspace) then 
+    if not Directory("Tests/" .. workspace) then
         os.execute("mkdir " .. "Tests/" .. workspace)
     end
-    
+
     if action == "set" then
         file = io.open(path, "w")
         io.output(file)
@@ -31,20 +32,16 @@ function CodeBlock(block)
     end
 end
 
---- Check if a file or directory exists in this path
 function Exists(file)
     local ok, err, code = os.rename(file, file)
     if not ok then
        if code == 13 then
-          -- Permission denied, but it exists
           return true
        end
     end
     return ok, err
  end
- 
- --- Check if a directory exists in this path
- function Directory(path)
-    -- "/" works on both Unix and Windows
+
+function Directory(path)
     return Exists(path.."/")
- end
+end
