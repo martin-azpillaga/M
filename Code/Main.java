be6c63d3defa0b82568c76a3da2163f977779fad
martin-azpillaga <martin.azpillaga@gmail.com>
2019-12-31 16:17:51 +0100

    Xtext generator runs with fat jar dependencies

--- Code/Main.java
+++ Code/Main.java
@@ -1,6 +1,33 @@
-public class Main {
+import org.eclipse.emf.mwe2.runtime.workflow.Workflow;
+import org.eclipse.xtext.xtext.generator.CodeConfig;
+import org.eclipse.xtext.xtext.generator.DefaultGeneratorModule;
+import org.eclipse.xtext.xtext.generator.XtextGenerator;
+import org.eclipse.xtext.xtext.generator.model.project.BundleProjectConfig;
+import org.eclipse.xtext.xtext.generator.model.project.StandardProjectConfig;
 
-    public static void main(final String[] args) {
-        System.out.println("Hello World!");
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        var generator = new XtextGenerator();
+        var configuration = new DefaultGeneratorModule();
+        generator.setConfiguration(configuration);
+
+        var code = new CodeConfig();
+        var project = new StandardProjectConfig();
+        configuration.setProject(project);
+        project.setCodeConfig(code);
+
+        project.setBaseName("Code");
+        project.setRootPath("..");
+        var ide = new BundleProjectConfig();
+        ide.setEnabled(true);
+        ide.setName("Code");
+        project.setGenericIde(ide);
+
+        code.setEncoding("UTF-8");
+        code.setLineDelimiter("\n");
+        System.out.println("Hello World");
+        generator.invoke(null);
     }
 }
