33334a45844336e99a82de1fb64cac78e49ac892
martin-azpillaga <martin.azpillaga@gmail.com>
2020-01-24 03:55:07 +0100

    Outline the serializers structure

--- Code/m/src/m/serializing/UnitySerializer.java
+++ Code/m/src/m/serializing/UnitySerializer.java
@@ -1,33 +1,42 @@
 package m.serializing;
 
+import static m.csharp.Visibility.PUBLIC;
+import static m.validation.Type.input;
+import static m.validation.Type.tag;
+
 import java.util.HashSet;
+import java.util.UUID;
 
 import org.eclipse.xtext.generator.IFileSystemAccess2;
 
 import m.CSharpRuntimeModule;
+import m.YAMLRuntimeModule;
 import m.csharp.CsharpFactory;
-import m.csharp.NamespaceMember;
 import m.csharp.NamespaceType;
-import m.csharp.Struct;
-import m.csharp.CompilationUnit;
-import static m.csharp.Visibility.*;
+import m.m.Archetype;
 import m.m.Game;
+import m.m.System;
 import m.modular.ModularFactory;
 import m.validation.MValidator;
 import m.validation.StandardLibrary;
 import m.validation.Type;
 import m.yaml.YamlFactory;
-import static m.validation.Type.*;
 
 public class UnitySerializer
 {
 	ModularFactory modular = ModularFactory.eINSTANCE;
 	CsharpFactory csharp = CsharpFactory.eINSTANCE;
 	YamlFactory yaml = YamlFactory.eINSTANCE;
+	CSharpRuntimeModule csharpModule;
+	YAMLRuntimeModule yamlModule;
+	IFileSystemAccess2 fsa;
 	
 	public void serialize(Game game, IFileSystemAccess2 fsa)
 	{
-		var csharpModule = new CSharpRuntimeModule();
+		csharpModule = new CSharpRuntimeModule();
+		yamlModule = new YAMLRuntimeModule();
+		this.fsa = fsa;
+		
 		var components = MValidator.components;
 		
 		for (var component : components.keySet())
@@ -39,17 +48,24 @@ public class UnitySerializer
 			catch (Exception exception)
 			{
 				var type = components.get(component);
-				
-				var file = serialize(component, type);
-				GenericSerializer.generate(file, csharpModule, fsa, "Unity/Assets/Code/Components/"+component+".cs");
-			
+				serialize(component, type);
 			}
+		}
+		
+		for (var archetype : game.getArchetypes())
+		{
+			serialize(archetype);
+		}
+		
+		for (var system : game.getSystems())
+		{
+			serialize(system);
 		}
 	}
 	
-	public CompilationUnit serialize(String component, Type type)
+	public void serialize(String component, Type type)
 	{
-		var model = csharp.createCompilationUnit();
+		var unit = csharp.createCompilationUnit();
 		
 		var namespaces = new HashSet<String>();
 		namespaces.add("Unity.Entities");
@@ -86,21 +102,87 @@ public class UnitySerializer
 			member.getMembers().add(valueField);
 		}
 		
+		if (type == input)
+		{
+			namespaces.add("Unity.Input");
+		}
+		
 		member.setVisibility(PUBLIC);
 		member.setName(component);
-		model.getTypes().add(member);
-		
-		
-		
-		
+		unit.getTypes().add(member);
+
 		for (var namespace : namespaces)
 		{
 			var using = csharp.createUsing();
 			using.setName(namespace);
-			model.getUsings().add(using);			
+			unit.getUsings().add(using);			
 		}
 		
-		return model;
+		var meta = yaml.createFile();
+		var fileFormat = yaml.createKeyValue();
+		fileFormat.setKey("fileFormatVersion");
+		var value = yaml.createFloat();
+		value.setValue("2");
+		fileFormat.setValue(value);
+		meta.getNodes().add(fileFormat);
+		
+		var guid = yaml.createKeyValue();
+		guid.setKey("guid");
+		var word = yaml.createGuid();
+		word.setValue(uuid(component+"component"));
+		guid.setValue(word);
+		meta.getNodes().add(guid);
+
+		GenericSerializer.generate(meta, yamlModule, fsa, "Unity/Assets/Code/Components/"+component+".cs.meta");
+
+		GenericSerializer.generate(unit, csharpModule, fsa, "Unity/Assets/Code/Components/"+component+".cs");
+	}
+	
+	private void serialize(Archetype archetype)
+	{
+		var file = yaml.createFile();
+		var guid = yaml.createKeyValue();
+		guid.setKey("guid");
+		var word = yaml.createGuid();
+		word.setValue(uuid(archetype.getName()+"component"));
+		guid.setValue(word);
+		file.getNodes().add(guid);
+		GenericSerializer.generate(file, yamlModule, fsa, "Unity/Assets/Design/Archetypes/"+archetype.getName()+".prefab");
+		
+		var meta = yaml.createFile();
+		var guid2 = yaml.createKeyValue();
+		guid.setKey("guid");
+		var word2 = yaml.createGuid();
+		word2.setValue(uuid(archetype.getName()+"component"));
+		guid2.setValue(word2);
+		meta.getNodes().add(guid);
+		GenericSerializer.generate(meta, yamlModule, fsa, "Unity/Assets/Design/Archetypes/"+archetype.getName()+".prefab.meta");
+	}
+	
+	private void serialize(System system)
+	{
+		var file = yaml.createFile();
+		var guid = yaml.createKeyValue();
+		guid.setKey("guid");
+		var word = yaml.createGuid();
+		word.setValue(uuid(system.getName()+"component"));
+		guid.setValue(word);
+		file.getNodes().add(guid);
+		GenericSerializer.generate(file, yamlModule, fsa, "Unity/Assets/Code/Systems/"+system.getName()+".prefab");
+		
+		var meta = yaml.createFile();
+		var guid2 = yaml.createKeyValue();
+		guid.setKey("guid");
+		var word2 = yaml.createGuid();
+		word2.setValue(uuid(system.getName()+"component"));
+		guid2.setValue(word2);
+		meta.getNodes().add(guid);
+		GenericSerializer.generate(meta, yamlModule, fsa, "Unity/Assets/Code/Systems/"+system.getName()+".prefab.meta");
+	}
+	
+	private String uuid(String string)
+	{
+		return UUID.nameUUIDFromBytes(string.getBytes()).toString().replace("-","");
 	}
 	
 	private String unityName(Type type)
@@ -118,7 +200,7 @@ public class UnitySerializer
 			case font: return "FontAsset";
 			case gameObject: return "GameObject";
 			case image: return "Texture";
-			case input: return "Unity.Input.PlayerInputAction";
+			case input: return "PlayerInputAction";
 			case material: return "Material";
 			case mesh: return "Mesh";
 			case none: return "None";
