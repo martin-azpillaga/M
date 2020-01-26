5eb1627cba577ad666ad17cc3324ad33cbd8b861
martin-azpillaga <martin.azpillaga@gmail.com>
2020-01-26 03:30:45 +0100

    Towards a solid YAML grammar

---
+++ Code/m/src/m/formatting/YAMLFormat.java
@@ -0,0 +1,14 @@
+package m.formatting;
+
+import m.yaml.File;
+
+public class YAMLFormat extends GenericFormatter
+{
+	@Override
+	protected void format(Object o)
+	{
+		if (o instanceof File)
+		{
+		}
+	}
+}
