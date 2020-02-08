0a0c00a8debf6ef5667c6033eaa037565d02acb7
martin-azpillaga <martin.azpillaga@gmail.com>
2020-02-08 23:47:01 +0100

    Csharp format

--- Code/m/src/m/formatting/JSONFormat.java
+++ Code/m/src/m/formatting/JSONFormat.java
@@ -13,32 +13,6 @@ public class JSONFormat extends GenericFormatter
 	@Override
 	public void format(Object obj) 
 	{
-		if (obj instanceof m.json.Object)
-		{
-			var o = (m.json.Object) obj;
-			var open = keyword(o,"{");
-			var close = keyword(o,"}");
-			if (o.eContainer() != null)
-			{
-				prepend(open, newLine());
-			}
-			prepend(close, newLine());
-			indent(open, close);
-
-			for (var comma : keywords(o,","))
-			{
-				prepend(comma, noSpace());
-			}
-			for (var member : o.getMembers())
-			{
-				format(member, document);
-			}
-		}
-		else if (obj instanceof Member)
-		{
-			var member = (Member) obj;
-			document.prepend(member, newLine());
-			format(member.getValue(),document);
-		}
+		
 	}	
 }
