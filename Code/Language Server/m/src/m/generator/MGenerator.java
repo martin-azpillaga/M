53b7540f192c532af4aad65c7a8b3bc0176a1f8f
Martin Azpillaga Aldalur <martin@pop-os.localdomain>
2020-02-21 16:10:55 +0100

    Language server, Language client and Application folders under Code

---
+++ Code/Language Server/m/src/m/generator/MGenerator.java
@@ -0,0 +1,24 @@
+package m.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.AbstractGenerator;
+import org.eclipse.xtext.generator.IFileSystemAccess2;
+import org.eclipse.xtext.generator.IGeneratorContext;
+
+import m.m.Game;
+import m.serializing.UnitySerializer;
+
+public class MGenerator extends AbstractGenerator 
+{
+	@Override
+	public void doGenerate(Resource resource, IFileSystemAccess2 fsa, IGeneratorContext context)
+	{
+		if (resource.getContents().size() == 0) return;
+		
+		var game = (Game) resource.getContents().get(0);
+		
+		var unitySerializer = new UnitySerializer();
+		
+		unitySerializer.serialize(game, fsa);
+	}
+}
