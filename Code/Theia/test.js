5ced1cf8b9469c45e0317e2c290871e4d57367bc
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2020-05-29 01:33:57 +0200

    Test with mocha and chai

--- Code/Theia/test.js
+++ Code/Theia/test.js
@@ -1,15 +1,34 @@
 const puppeteer = require('puppeteer');
+const chai = require('chai');
+chai.should();
 
 let browser;
 
-(async () =>
+describe('The application', function()
 {
-        browser = await puppeteer.launch({headless: false, slowMo: 1000});
-        const pages = await browser.pages();
-        const page = pages[0];
-        await page.goto('localhost:3000');
-        await page.click('#shell-tab-explorer-view-container')
-        await page.screenshot({path: 'example.png'});
-        await browser.close();
-    
-})().catch(err => {console.log("\n\n"+err+"\n\n"); browser.close(); process.exit(111)});
+    this.timeout(10000);
+
+    before(async function ()
+    {
+        browser = await puppeteer.launch({headless: false, slowMo: 500});
+    })
+    it('Opens a page', async function ()
+    {
+        try
+        {
+            const pages = await browser.pages();
+            const page = pages[0];
+            await page.goto('localhost:3000');
+            await page.click('#shell-tab-explorer-view-container')
+            await page.screenshot({path: 'example.png'});
+            await browser.close();
+            (2+2).should.equal(4)
+        }
+        catch (err)
+        {
+            console.log("\n\n"+err+"\n\n");
+            browser.close();
+            process.exit(111);
+        }
+    })
+})
