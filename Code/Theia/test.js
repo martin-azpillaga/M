9a8ad62589eedf14fc58260c7fb915ac109e202f
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2020-05-29 01:53:26 +0200

    Run tests in isolated incognito contexts instead of isolated browsers for performance

--- Code/Theia/test.js
+++ Code/Theia/test.js
@@ -3,6 +3,7 @@ const chai = require('chai');
 chai.should();
 
 let browser;
+let context;
 
 describe('The application', function()
 {
@@ -12,22 +13,36 @@ describe('The application', function()
     {
         browser = await puppeteer.launch({headless: false, slowMo: 500});
     })
+    after(async function ()
+    {
+        if (!!browser)
+        {
+            browser.close();
+        }
+    })
+    beforeEach(async function()
+    {
+        context = await browser.createIncognitoBrowserContext();
+    })
+    afterEach(async function()
+    {
+        await context.close();
+    })
     it('Opens a page', async function ()
     {
         try
         {
-            const pages = await browser.pages();
-            const page = pages[0];
+            const page = await context.newPage();
             await page.goto('localhost:3000');
             await page.click('#shell-tab-explorer-view-container')
             await page.screenshot({path: 'example.png'});
-            await browser.close();
+            
             (2+2).should.equal(4)
         }
         catch (err)
         {
             console.log("\n\n"+err+"\n\n");
-            browser.close();
+            context.close();
             process.exit(111);
         }
     })
