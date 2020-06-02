71ec240cbd36abb6c6333987db25fb1c624f25df
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2020-06-02 03:33:50 +0200

    Wait until the file name is opened before typing in it

--- Code/Theia/generated.js
+++ Code/Theia/generated.js
@@ -1,7 +1,8 @@
-const { click, type, openBrowser, closeBrowser, connectToServer, closeContext} = require('./mobot.js')
+const { click, type, isVisible, openBrowser, closeBrowser, connectToServer, closeContext} = require('./mobot.js')
 
 describe('test', function()
 {
+    // run yarn startBrowser workspace & in mobot.before()
     this.timeout(100000)
     before(openBrowser)
     beforeEach(connectToServer)
@@ -14,5 +15,8 @@ describe('test', function()
         await click("New File");
         await type("Hello.m");
         await click("OK");
+        await isVisible("Hello.m");
+        await type("Hello World!");
+        await click("View");
     })
 })
