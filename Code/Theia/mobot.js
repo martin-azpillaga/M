663f3477c30c6e19f8e43ab0d866d7bdc41dd785
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2020-06-02 02:57:20 +0200

    Include mobot.js

---
+++ Code/Theia/mobot.js
@@ -0,0 +1,80 @@
+const puppeteer = require('puppeteer');
+const chai = require('chai');
+const rimraf = require('rimraf');
+
+chai.should();
+
+let browser;
+let context;
+let page;
+
+async function type(text)
+{
+    var lines = text.split("\n");
+    for (line of lines)
+    {
+        await page.keyboard.type(line);
+        if (line !== lines[lines.length-1])
+        {
+            await page.keyboard.press("Escape");
+            await page.keyboard.type("\n");
+        }
+    }
+}
+before(async function ()
+{
+    var slow = process.argv[3] == "slow";
+    if (slow)
+    {
+        browser = await puppeteer.launch({headless: false, slowMo: 100, defaultViewport: null, args: ['--start-maximized']});
+    }
+    else
+    {
+        browser = await puppeteer.launch({headless: false});
+    }  
+})
+after(async function ()
+{
+    if (!!browser)
+    {
+        browser.close();
+    }
+})
+beforeEach(async function()
+{
+    context = await browser.createIncognitoBrowserContext();
+    page = await context.newPage();
+    rimraf.sync('workspace/*');
+})
+afterEach(async function()
+{
+    if (!!context)
+    {
+        await context.close();
+    }
+})
+
+async function click(path)
+{
+    const fullPath = '//*[text()="'+path+'"]'
+    await page.waitForXPath(fullPath);
+    var elements = await page.$x(fullPath);
+    if (elements.length == 1)
+    {
+        const element = elements[0];
+        await element.click();
+    }
+    else
+    {
+        throw new Error(elements.length + " elements found with path "+path);
+    }
+}
+
+async function isVisible(path)
+{
+    const fullPath = '//*[text()="'+path+'"]'
+    var element = await page.waitForXPath(fullPath, {timeout: 5000});
+    return !!element
+}
+
+module.exports = {click, isVisible, type, browser, page, context}
