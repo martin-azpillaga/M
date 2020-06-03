378910fa31c9a24550d974b25d8f5d885fb6a547
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2020-06-03 02:53:12 +0200

    Previous paragraph sets the title of each test in a file

--- Code/Theia/test.js
+++ Code/Theia/test.js
@@ -1,178 +1,25 @@
-const puppeteer = require('puppeteer');
-const chai = require('chai');
-const rimraf = require('rimraf');
+const { click, type, isVisible, openBrowser, closeBrowser, connectToServer, closeContext} = require('./mobot.js')
 
-chai.should();
-
-let browser;
-let context;
-let page;
-
-async function clickSelector(selector)
+describe('Test', function()
 {
-    var selected = await page.waitForSelector(selector);
-    await selected.click();
-}
+this.timeout(100000)
+before(openBrowser)
+beforeEach(connectToServer)
+afterEach(closeContext)
+after(closeBrowser)
 
-async function clickPath(path)
+it('Some explanation', async function()
 {
-    await page.waitForXPath(path);
-    var elements = await page.$x(path);
-    if (elements.length == 1)
-    {
-        const element = elements[0];
-        await element.click();
-    }
-    else
-    {
-        for (element of elements)
-        {
-            console.log(element)
-        }
-        throw new Error(elements.length + " elements found with path "+path);
-    }
-}
-
-async function type(text)
-{
-    var lines = text.split("\n");
-    for (line of lines)
-    {
-        await page.keyboard.type(line);
-        if (line !== lines[lines.length-1])
-        {
-            await page.keyboard.press("Escape");
-            await page.keyboard.type("\n");
-        }
-    }
-}
-describe('The application', function()
-{
-    this.timeout(100000);
-
-    before(async function ()
-    {
-        var slow = process.argv[3] == "slow";
-        if (slow)
-        {
-            browser = await puppeteer.launch({headless: false, slowMo: 100, defaultViewport: null, args: ['--start-maximized']});
-        }
-        else
-        {
-            browser = await puppeteer.launch({headless: false});
-        }  
-    })
-    after(async function ()
-    {
-        if (!!browser)
-        {
-            browser.close();
-        }
-    })
-    beforeEach(async function()
-    {
-        context = await browser.createIncognitoBrowserContext();
-        page = await context.newPage();
-        rimraf.sync('workspace/*');
-    })
-    afterEach(async function()
-    {
-        if (!!context)
-        {
-            await context.close();
-        }
-    })
-    /*
-    it('Opens a page', async function ()
-    {
-        try
-        {
-            
-            // Connect to localhost:3000/#/workspace-path?
-            // Use puppeteer-recorder https://www.npmjs.com/package/puppeteer-recorder
-            // to record animations of tests
-            await page.goto('localhost:3000');
-            await clickPath('//div[text()="File"]')
-            await clickPath('//div[text()="New File"]')
-            await type("Hello.m");
-            await clickPath('//button[text()="OK"]');
-            await page.waitForSelector(".view-line");
-            await type("control()\n{}\n");
-            await clickPath('//div[text()="View"]');
-            await clickPath('//div[@class="p-Menu-itemLabel" and text()="Problems"]');
-            await page.waitForXPath('//div[text()="No problems have been detected in the workspace so far."]');
-        }
-        catch (err)
-        {
-            console.error("\n\n"+err+"\n\n");
-            process.exit(111);
-        }
-    })*/
-
-    it('Creates a workspace', async function ()
-    {
-        try
-        {
-            await page.goto('localhost:3000');
-
-            const workspace = "Hello"
-
-            await click("File");
-            if (await isVisible("New Folder"))
-            {
-                await click("New Folder");
-                await type(workspace);
-                await click("OK");
-    
-                await click("File");
-                await click("Open Workspace...");
-                await click(workspace);
-                await click("Open");
-            }
-
-            /*
-            await page.waitFor(5000);
-            const pages = await browser.pages()
-            page = pages[1];
-            await clickPath('//*[text()="File"]')
-            await clickPath('//*[text()="New File"]')
-            await type("Hello.m");
-            await clickPath('//*[text()="OK"]');
-
-            await page.waitForSelector(".view-line");
-            await type("control()\n{}\n");
-            await clickPath('//div[text()="View"]');
-            await clickPath('//div[@class="p-Menu-itemLabel" and text()="Problems"]');
-            await page.waitForXPath('//div[text()="No problems have been detected in the workspace so far."]');
-            */
-        }
-        catch (err)
-        {
-            console.error("\n\n"+err+"\n\n");
-            process.exit(111);
-        }
-    })
+await click('File');
+await click('New File');
+await type('Hello.m');
+await click('OK');
+await isVisible('Hello.m');
+await type('Hello World!');
 })
-
-async function click(path)
+it('Later on', async function()
 {
-    const fullPath = '//*[text()="'+path+'"]'
-    await page.waitForXPath(fullPath);
-    var elements = await page.$x(fullPath);
-    if (elements.length == 1)
-    {
-        const element = elements[0];
-        await element.click();
-    }
-    else
-    {
-        throw new Error(elements.length + " elements found with path "+path);
-    }
-}
-
-async function isVisible(path)
-{
-    const fullPath = '//*[text()="'+path+'"]'
-    var element = await page.waitForXPath(fullPath, {timeout: 5000});
-    return !!element
-}
+await click('c');
+await click('d');
+})
+})
