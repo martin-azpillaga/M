3477b99507dfc9088b878d66bccc6d11cd2ea481
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2020-06-03 03:28:06 +0200

    Enable multiple test suites if multiple titles are present in markdown

--- Code/Theia/test.js
+++ Code/Theia/test.js
@@ -1,5 +1,35 @@
 const { click, type, isVisible, openBrowser, closeBrowser, connectToServer, closeContext} = require('./mobot.js')
 
+describe('Authors', function()
+{
+this.timeout(100000)
+before(openBrowser)
+beforeEach(connectToServer)
+afterEach(closeContext)
+after(closeBrowser)
+
+it('The  framework powers the online and standalone textual editors.TypeFox licenses Theia under the .', async function()
+{
+})
+})
+describe('What is M', function()
+{
+this.timeout(100000)
+before(openBrowser)
+beforeEach(connectToServer)
+afterEach(closeContext)
+after(closeBrowser)
+
+it('M does not compete with the tools you know and love. It provides you with atool that will improve your development journey.', async function()
+{
+})
+it('Missing a feature? Open an issue.', async function()
+{
+})
+it('You can donate using GitHub’s built-in sponsorship feature. Click the sponsorheart button and set the amount you feel comfortable with.', async function()
+{
+})
+})
 describe('Test', function()
 {
 this.timeout(100000)
@@ -19,7 +49,9 @@ await type('Hello World!');
 })
 it('Later on', async function()
 {
-await click('c');
-await click('d');
+await click('File');
+await click('New File');
+await type('Hello.m');
+await click('OK');
 })
 })
