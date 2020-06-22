0bab8ac71797ae6040cd588f9b9a7513755ff6bd
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-06-22 14:48:47 +0200

    Embed pro grammar in M

--- runtime-EclipseXtext/Testing/src-gen/Godot/Code/Systems/restart.gd
+++ runtime-EclipseXtext/Testing/src-gen/Godot/Code/Systems/restart.gd
@@ -8,7 +8,6 @@ func _process(delta):
 		var maxNumber_a = a["maxNumber"];
 		if number_a.Value > maxNumber_a.Value:
 			for b in root.entities(["number","resetNumber"]):
-				print("restart")
 				var entity_b = b["entity"]
 				var number_b = b["number"];
 				var resetNumber_b = b["resetNumber"];
