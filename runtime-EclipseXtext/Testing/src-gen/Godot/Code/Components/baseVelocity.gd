c5f19a2d1e335d80cfe690979c8f9892a74bc718
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-06-23 23:30:04 +0200

    Save before trip

--- runtime-EclipseXtext/Testing/src-gen/Godot/Code/Components/baseVelocity.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-export var Value: Vector3
-
-func get_class(): return "baseVelocity"
-func is_class(type): return type == get_class() || .is_class(type)
--- runtime-EclipseXtext/Testing/src-gen/Godot/Code/Systems/control.gd
+++ runtime-EclipseXtext/Testing/src-gen/Godot/Code/Systems/control.gd
@@ -2,9 +2,8 @@ extends Node
 
 func _process(delta):
 	var root = get_tree().get_root().get_child(0)
-	for a in root.entities(["inputValue","baseVelocity","RigidBody"]):
+	for a in root.entities(["RigidBody","position"]):
 		var entity_a = a["entity"]
-		var inputValue_a = a["inputValue"];
-		var baseVelocity_a = a["baseVelocity"];
 		var velocity_a = a["RigidBody"];
-		velocity_a.linear_velocity = baseVelocity_a.Value * inputValue_a.Value
+		var position_a = a["position"];
+		velocity_a.linear_velocity = position_a.Value
