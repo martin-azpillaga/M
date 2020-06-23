14444da3b2249cfa2f47aa52f98e493eb75cb486
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-06-23 23:30:04 +0200

    Save before trip

--- runtime-EclipseXtext/Testing/src-gen/Godot/Code/Systems/respawn.gd
+++ runtime-EclipseXtext/Testing/src-gen/Godot/Code/Systems/respawn.gd
@@ -2,18 +2,12 @@ extends Node
 
 func _process(delta):
 	var root = get_tree().get_root().get_child(0)
-	for a in root.entities(["Spatial","RigidBody"]):
+	for a in root.entities(["template"]):
 		var entity_a = a["entity"]
-		var position_a = a["Spatial"];
-		var velocity_a = a["RigidBody"];
-		for b in root.entities(["collisions","respawnPoint","respawnVelocity"]):
+		var template_a = a["template"];
+		for b in root.entities(["collisions"]):
 			var entity_b = b["entity"]
 			var collisions_b = b["collisions"];
-			var respawnPoint_b = b["respawnPoint"];
-			var respawnVelocity_b = b["respawnVelocity"];
 			if entity_a in collisions_b.Value:
-				position_a.translation = respawnPoint_b.Value
-				
-				velocity_a.linear_velocity = respawnVelocity_b.Value
-				
-				entity_a.add_child(preload("res://Engine/timeout.gd").new());
+				root.get_node("Entities").add_child(load(template_a.Value).instance());
+				entity_a.queue_free();
