e99f66e98fd6f12a86f62b1addbe09da4898a272
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-07-03 00:25:15 +0200

    Clean up Godot files

--- runtime-EclipseXtext/Testing/src-gen/Godot/Code/Components/serveLength.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-export var Value: Vector2
-
-func get_class(): return "serveLength"
-func is_class(type): return type == get_class() || .is_class(type)
--- runtime-EclipseXtext/Testing/src-gen/Godot/Code/Systems/control.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func _process(delta):
-	var root = get_tree().get_root().get_child(0)
-	for a in root.entities(["RigidBody","position"]):
-		var entity_a = a["entity"]
-		var velocity_a = a["RigidBody"];
-		var position_a = a["position"];
-		velocity_a.linear_velocity = position_a.Value
