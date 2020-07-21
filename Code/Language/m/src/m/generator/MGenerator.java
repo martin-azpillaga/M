4e2962cc4a73aa51280d592c3ac8b7bc37011748
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-07-21 18:50:36 +0200

    Separation of concerns Library > Validator > Context > TypingRules > Inference > Game

--- Code/Language/m/src/m/generator/MGenerator.java
+++ Code/Language/m/src/m/generator/MGenerator.java
@@ -5,17 +5,21 @@ import org.eclipse.xtext.generator.AbstractGenerator;
 import org.eclipse.xtext.generator.IFileSystemAccess2;
 import org.eclipse.xtext.generator.IGeneratorContext;
 
+import com.google.inject.Inject;
+
 import m.validation.MValidator;
 
 public class MGenerator extends AbstractGenerator
 {
+	@Inject
+	MValidator validator;
+	
 	public void doGenerate(Resource resource, IFileSystemAccess2 fsa, IGeneratorContext context)
 	{
-		var game = MValidator.game;
+		var game = validator.getGame();
 		if (game != null)
 		{
-			new Unity().generate(game,fsa);
-			new Godot().generate(game,fsa);
+			
 		}
 	}
 }
