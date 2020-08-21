02a324b279799d95a6763482eae6ef8ee55d8148
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-08-21 02:14:11 +0200

    Simple activate and deactivate callbacks

--- Code/VSCode/extension.js
+++ Code/VSCode/extension.js
@@ -1,1 +1,10 @@
-console.log("Hello extension");
+var {join} = require("path");
+
+exports.activate = function (context)
+{
+    console.log("activate"+join("a", "b"));
+}
+exports.deactivate = function ()
+{
+    console.log("deactivate");
+}
