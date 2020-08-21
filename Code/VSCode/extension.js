0ee957f7b0b62edda01911bb65ab7d5c99e7f7e8
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-08-21 02:56:56 +0200

    Start language server process with java -jar

--- Code/VSCode/extension.js
+++ Code/VSCode/extension.js
@@ -1,10 +1,42 @@
 var {join} = require("path");
+var {LanguageClient} = require("vscode-languageclient");
+var { workspace } = require('vscode');
+const { spawn } = require('child_process')
+
+var client;
 
 exports.activate = function (context)
 {
-    console.log("activate"+join("a", "b"));
+    console.log(context.asAbsolutePath("ls.jar"));
+
+    
+    var serverOptions =
+    {
+        run:
+        {
+            command: "java",
+            args:["-jar", context.asAbsolutePath("ls.jar")]
+        }
+    };
+
+    var clientOptions =
+    {
+        documentSelector: [{ scheme: 'file', language: 'm' }],
+        synchronize:
+        {
+            fileEvents: workspace.createFileSystemWatcher('**/.m')
+        }
+    };
+
+    client = new LanguageClient('mserver', 'mserver example', serverOptions, clientOptions);
+
+    client.start();
 }
 exports.deactivate = function ()
 {
-    console.log("deactivate");
+    if (!client)
+    {
+        return undefined;
+    }
+    client.stop();
 }
