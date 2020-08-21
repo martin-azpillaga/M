825681cec12adadb72d07e54c0f03a6566745ac9
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-08-21 23:06:53 +0200

    Java 8 target compatible code

--- Code/VSCode/extension.js
+++ Code/VSCode/extension.js
@@ -1,15 +1,10 @@
-var {join} = require("path");
-var {LanguageClient} = require("vscode-languageclient");
+var { LanguageClient } = require("vscode-languageclient");
 var { workspace } = require('vscode');
-const { spawn } = require('child_process')
 
 var client;
 
 exports.activate = function (context)
 {
-    console.log(context.asAbsolutePath("ls.jar"));
-
-    
     var serverOptions =
     {
         run:
@@ -28,7 +23,7 @@ exports.activate = function (context)
         }
     };
 
-    client = new LanguageClient('mserver', 'mserver example', serverOptions, clientOptions);
+    client = new LanguageClient('mserver', 'm language server', serverOptions, clientOptions);
 
     client.start();
 }
