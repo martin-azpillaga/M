b46ee0a8b8ab66df07f02133c1b67fc9659229c8
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-08-24 01:29:13 +0200

    Generate manifest.json if non existent

--- Code/Language/m/src/m/generator/ClassicUnity.java
+++ Code/Language/m/src/m/generator/ClassicUnity.java
@@ -79,6 +79,8 @@ public class ClassicUnity
 		this.variables = new HashSet<String>();
 		this.stack = new Stack<>();
 		
+		resolvePackages();
+		
 		for (var component : game.components.entrySet())
 		{
 			fileSystem.generateFile("Assets/Code/Components/"+unreserved(component.getKey())+".cs",
@@ -141,6 +143,66 @@ public class ClassicUnity
 		systems += "}\n}\n}\n";
 		
 		fileSystem.generateFile("Assets/Code/Systems/Systems.cs", systems);
+	}
+	
+	private void resolvePackages()
+	{
+		if (fileSystem.isFile("Packages/manifest.json"))
+		{
+			var manifest = fileSystem.readTextFile("Packages/manifest.json").toString();
+			var dependencies = "\"dependencies\": {";
+			if (manifest.contains(dependencies))
+			{
+				var index = manifest.indexOf("\"dependencies\"");
+			}
+		}
+		else
+		{
+			fileSystem.generateFile("Packages/manifest.json", lines
+			(
+			"",
+			"{",
+			"  \"dependencies\": {",
+			"    \"com.unity.entities\": \"0.11.1-preview.4\",",
+			"    \"com.unity.inputsystem\": \"1.0.0\",",
+            "    \"com.unity.ugui\": \"1.0.0\",",  
+            "    \"com.unity.modules.ai\": \"1.0.0\",",  
+            "    \"com.unity.modules.androidjni\": \"1.0.0\",",  
+            "    \"com.unity.modules.animation\": \"1.0.0\",",  
+            "    \"com.unity.modules.assetbundle\": \"1.0.0\",",  
+            "    \"com.unity.modules.audio\": \"1.0.0\",",  
+            "    \"com.unity.modules.cloth\": \"1.0.0\",",  
+            "    \"com.unity.modules.director\": \"1.0.0\",",  
+            "    \"com.unity.modules.imageconversion\": \"1.0.0\",",  
+            "    \"com.unity.modules.imgui\": \"1.0.0\",",  
+            "    \"com.unity.modules.jsonserialize\": \"1.0.0\",",  
+            "    \"com.unity.modules.particlesystem\": \"1.0.0\",",  
+            "    \"com.unity.modules.physics\": \"1.0.0\",",  
+            "    \"com.unity.modules.physics2d\": \"1.0.0\",",  
+            "    \"com.unity.modules.screencapture\": \"1.0.0\",",  
+            "    \"com.unity.modules.terrain\": \"1.0.0\",",  
+            "    \"com.unity.modules.terrainphysics\": \"1.0.0\",",  
+            "    \"com.unity.modules.tilemap\": \"1.0.0\",",  
+            "    \"com.unity.modules.ui\": \"1.0.0\",",  
+            "    \"com.unity.modules.uielements\": \"1.0.0\",",  
+            "    \"com.unity.modules.umbra\": \"1.0.0\",",  
+            "    \"com.unity.modules.unityanalytics\": \"1.0.0\",",  
+            "    \"com.unity.modules.unitywebrequest\": \"1.0.0\",",  
+            "    \"com.unity.modules.unitywebrequestassetbundle\": \"1.0.0\",",  
+            "    \"com.unity.modules.unitywebrequestaudio\": \"1.0.0\",",  
+            "    \"com.unity.modules.unitywebrequesttexture\": \"1.0.0\",",  
+            "    \"com.unity.modules.unitywebrequestwww\": \"1.0.0\",",  
+            "    \"com.unity.modules.vehicles\": \"1.0.0\",",  
+            "    \"com.unity.modules.video\": \"1.0.0\",",  
+            "    \"com.unity.modules.vr\": \"1.0.0\",",  
+            "    \"com.unity.modules.wind\": \"1.0.0\",",  
+            "    \"com.unity.modules.xr\": \"1.0.0\"",
+			"  }",
+			"}"
+			));
+		}
+		
+		
 	}
 	
 	private String typeOf(String component)
