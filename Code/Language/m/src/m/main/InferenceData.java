7bb7580df86416ad301df3954145748d4c1f8b39
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-08-25 19:21:27 +0200

    Code generation with multiple files (without workspace edits)

--- Code/Language/m/src/m/main/InferenceData.java
+++ Code/Language/m/src/m/main/InferenceData.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import m.m.Function;
 import m.validation.problems.Problem;
 import m.validation.rules.ExpressionNode;
 
@@ -12,5 +13,6 @@ public class InferenceData
 {
 	public List<ExpressionNode> nodes = new ArrayList<>();
 	public Map<String, ExpressionNode> components = new HashMap<>();
+	public Map<String, Function> functions = new HashMap<>();
 	public List<Problem> problems = new ArrayList<>();
 }
