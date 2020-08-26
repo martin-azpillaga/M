2b3780218533050ec8334697009ed19bc9154882
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-08-26 03:20:07 +0200

    Clean linting and files

---
+++ Code/Language/m/src/m/main/CrossReference.java
@@ -0,0 +1,15 @@
+package m.main;
+
+import m.validation.rules.Binding;
+import m.validation.rules.ExpressionNode;
+
+public class CrossReference
+{
+	public String fileA;
+	public ExpressionNode nodeA;
+	public Binding bindingA;
+	
+	public String fileB;
+	public ExpressionNode nodeB;
+	public Binding bindingB;
+}
