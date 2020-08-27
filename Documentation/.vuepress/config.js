39733866a46bec705b91342bfff8dbfc00a94196
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-08-27 15:57:53 +0200

    Clean files

--- Documentation/.vuepress/config.js
+++ Documentation/.vuepress/config.js
@@ -3,33 +3,39 @@ module.exports = {
     description: 'A data-driven programming language for game developers',
     theme: 'yuu',
     themeConfig: {
-      logo: 'https://avatars2.githubusercontent.com/u/6147446?s=400&u=c325f5bfc187e889f05cc872681ad1cb1ba6b3b5&v=4',
-      nav: [
-        { text: 'Starter guide', link: '/0. Starter guide/0. Set up' },
-        { text: 'User guide', link: '/1. User guide/0. How to/0. Transforms' },
-        { text: 'Developer guide', link: '/2. Developer guide/0. Philosophy/0. Motivation' }
-      ],
-      sidebar: {
-        '/0. Starter guide/': [
-          '0. Set up',
-          '1. Your first project',
-          '2. Personalize'
+        logo: 'https://avatars2.githubusercontent.com/u/6147446?s=400&u=c325f5bfc187e889f05cc872681ad1cb1ba6b3b5&v=4',
+        nav: [{
+                text: 'Starter guide',
+                link: '/0. Starter guide/0. Set up'
+            },
+            {
+                text: 'User guide',
+                link: '/1. User guide/0. How to/0. Transforms'
+            },
+            {
+                text: 'Developer guide',
+                link: '/2. Developer guide/0. Philosophy/0. Motivation'
+            }
         ],
-        '/1. User guide/': [
-          {
-            title: 'How to',
-            children: [
-              '0. How to/0. Transforms',
-              '0. How to/1. Rendering'
+        sidebar: {
+            '/0. Starter guide/': [
+                '0. Set up',
+                '1. Your first project',
+                '2. Personalize'
+            ],
+            '/1. User guide/': [{
+                title: 'How to',
+                children: [
+                    '0. How to/0. Transforms',
+                    '0. How to/1. Rendering'
+                ]
+            }],
+            '/2. Developer guide/': [
+                '0. Philosophy/0. Motivation',
+                '0. Philosophy/2. Design decisions'
             ]
-          }
-        ],
-        '/2. Developer guide/': [
-          '0. Philosophy/0. Motivation',
-          '0. Philosophy/2. Design decisions'
-        ]
-  
-      },
-      lastUpdated: 'Last Updated'
+
+        },
+        lastUpdated: 'Last Updated'
     }
-  }
+}
