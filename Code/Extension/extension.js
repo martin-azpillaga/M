6f87deb51aab1a265a1460098e4c88e4b24a4ca3
Martin Azpillaga <martin.azpillaga@gmail.com>
2020-09-08 19:18:27 +0200

    Open webview in vscode extension

--- Code/Extension/extension.js
+++ Code/Extension/extension.js
@@ -1,5 +1,7 @@
 const {
-    window
+    window,
+    commands,
+    ViewColumn
 } = require("vscode");
 const {
     lookpath
@@ -16,7 +18,19 @@ var client;
 
 exports.activate = async function(context) {
 
-    var java = await lookpath("javaccc");
+    context.subscriptions.push(
+        commands.registerCommand('m.open', () => {
+            const panel = window.createWebviewPanel(
+                'mVisual', // Identifies the type of the webview. Used internally
+                'M Blocks', // Title of the panel displayed to the user
+                ViewColumn.One, // Editor column to show the new webview panel in.
+                {} // Webview options. More on these later.
+              );
+            panel.webview.html = blocksHtml();
+        })
+    );
+
+    var java = await lookpath("java");
     if (!java) {
         var selection = window.showInformationMessage("M requires a Java 8+ runtime to execute. Please install and add Java 8+ to the path", "Install Java 8+");
         selection.then(x => require("openurl").open("https://jdk.java.net/14/"));
@@ -49,4 +63,19 @@ exports.deactivate = function() {
         return undefined;
     }
     client.stop();
+}
+
+function blocksHtml()
+{
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+</body>
+</html>`;
 }
